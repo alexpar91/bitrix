3ffe7763aac4f4a3b13a6317596c9d7c12572869
Alexander Paramonov <alexi91@yandex.ru>
2022-11-09 23:19:54 +0300

    start

---
+++ test.php
@@ -0,0 +1,25 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("Новая страница");
+?><br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+    <br><br><br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br><? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
